fa58a93e6d9b06da9966ddba1bf1250b557feff3
TaffleDev <casperseidelin2@gmail.com>
2025-02-09 02:39:05 +0100

    forgot an update to player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("Components")]
-    PlayerHealth playerHealth;
+    private PlayerHealth playerHealth;
 
     private void Awake()
     {
